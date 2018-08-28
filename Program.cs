3d04996efd199b378c070f12c06a32b95e6b061f
Robert Leedy <leedyrj@gmail.com>
2018-08-28 11:19:26 -0500

    finished nickleback exercise

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nickleback
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<(string, string)> goodSongs = new List<(string, string)>();
+
+            HashSet<(string, string)> allSongs = new HashSet<(string, string)>(){
+                ("In My Life", "The Beatles"),
+                ("Ways to Go", "Grouplove"),
+                ("Gold Rush", "Deathcab for Cutie"),
+                ("Something Human", "Muse"),
+                ("Photographs", "Nickleback"),
+                ("Some Other Song by Nickleback", "Nickleback"),
+                ("Rut", "The Killers")
+            };
+
+            foreach ((string, string) song in allSongs)
+            {
+                if (song.Item2 != "Nickleback")
+                {
+                    goodSongs.Add(song);
+                }
+            }
+            foreach ((string, string) item in goodSongs)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
